45eb640e2c57a9ea8786bf7897a353745f2981b3
Kozi <steph4140@gmail.com>
2025-10-08 16:19:14 +0200

    feat Update viewModel tests for better coverage

--- app/src/test/java/com/android/wildex/ui/home/HomeScreenViewModelTest.kt
+++ app/src/test/java/com/android/wildex/ui/home/HomeScreenViewModelTest.kt
@@ -1,12 +1,16 @@
 package com.android.wildex.ui.home
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertNull
+import org.junit.Rule
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class HomeScreenViewModelTest {
     private lateinit var viewModel: HomeScreenViewModel
 
@@ -17,10 +21,6 @@ class HomeScreenViewModelTest {
     @After
     fun tearDown() {}
     @Test
-    fun testExample() {
-        assertNotNull(viewModel.uiState.value)
-    }
-    @Test
     fun `viewModel initializes default UI state`(){
         val initialState = viewModel.uiState.value
         assertEquals(emptyList<com.android.wildex.model.social.Post>(), initialState.posts)
@@ -28,5 +28,15 @@ class HomeScreenViewModelTest {
         assertEquals(false, initialState.notif)
         assertNull(initialState.errorMsg)
         assertEquals(false, initialState.signedOut)
+    }
+    @Test
+    fun `refreshUIState updates UI state`(){
+        viewModel.refreshUIState()
+        val updatedState = viewModel.uiState.value
+        assertEquals(emptyList<com.android.wildex.model.social.Post>(), updatedState.posts)
+        assertNull(updatedState.user)
+        assertEquals(false, updatedState.notif)
+        assertNull(updatedState.errorMsg)
+        assertEquals(false, updatedState.signedOut)
     }
 }

ff8a3c4309d632df3052e493dad6ddc66f263b09
Martin Zimmer <mzimmer@ic2-adrstd-5002.intranet.epfl.ch>
2025-10-09 11:33:16 +0200

    fix: Format Code to ktfmt

--- app/src/main/java/com/android/wildex/MainActivity.kt
+++ app/src/main/java/com/android/wildex/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.android.wildex.ui.home.HomeScreen
 import com.android.wildex.ui.theme.WildexTheme
 import okhttp3.OkHttpClient
 
@@ -20,7 +21,7 @@ object HttpClientProvider {
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContent { WildexTheme { Surface(modifier = Modifier.fillMaxSize()) { WildexApp() } } }
+    setContent { WildexTheme { Surface(modifier = Modifier.fillMaxSize()) { HomeScreen() } } }
   }
 }
 
--- app/src/main/java/com/android/wildex/ui/home/HomeScreen.kt
+++ app/src/main/java/com/android/wildex/ui/home/HomeScreen.kt
@@ -1,14 +1,11 @@
 package com.android.wildex.ui.home
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -37,7 +34,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -55,247 +51,189 @@ import com.google.firebase.Timestamp
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-//TODO: Remove those hardcoded values
+// TODO: Remove those hardcoded values
 val Crimson = Color(112, 38, 50)
-val post = Post(
-    "1",
-    "Alice",
-    "",
-    Location(.0, .0),
-    Timestamp.now(),
-    "a Dog",
-    12,
-    3
-)
+val post = Post("1", "Alice", "", Location(.0, .0), Timestamp.now(), "a Dog", 12, 3)
 val posts: MutableList<Post> = mutableListOf()
-val messageTextStyle = TextStyle(
-    color = Crimson,
-    fontWeight = FontWeight.Bold,
-    fontSize = 25.sp
-)
-val profilePicture = "https://www.shutterstock.com/image-photo/" +
+val messageTextStyle = TextStyle(color = Crimson, fontWeight = FontWeight.Bold, fontSize = 25.sp)
+val profilePicture =
+    "https://www.shutterstock.com/image-photo/" +
         "handsome-happy-african-american-bearded-600nw-2460702995.jpg"
-val authorPicture = "https://paulhollandphotography.com/cdn/shop/articles/" +
+val authorPicture =
+    "https://paulhollandphotography.com/cdn/shop/articles/" +
         "4713_Individual_Outdoor_f930382f-c9d6-4e5b-b17d-9fe300ae169c.jpg?v=1743534144&width=1500"
-val picture = "https://hips.hearstapps.com/hmg-prod/images/" +
+val picture =
+    "https://hips.hearstapps.com/hmg-prod/images/" +
         "cute-baby-animals-1558535060.jpg?crop=0.752xw:1.00xh;0.125xw,0&resize=640:*"
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(){
-    //TODO: Remove when viewModel implemented
-    for (i in 1..0){
-        posts.add(post)
-    }
+fun HomeScreen() {
+  // TODO: Remove when viewModel implemented
+  for (i in 1..5) {
+    posts.add(post)
+  }
 
-    Scaffold (
-        topBar = {
-            TopAppBar(
-                title = {
-                    Box(
-                        modifier = Modifier.fillMaxWidth(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            text = "Wildex",
-                            style = MaterialTheme.typography.titleLarge.copy(
-                                fontWeight = FontWeight.Bold,
-                                letterSpacing = 1.sp
-                            )
-                        )
-                    }
-                },
-                navigationIcon = {
-                    IconButton(
-                        onClick = { /*TODO: Implement Notifications Screen*/ }
-                    ) {
-                        Icon(
-                            painter = painterResource(R.drawable.notification_bell),
-                            contentDescription = "Notifications",
-                            modifier = Modifier.size(30.dp),
-                        )
-                    }
-                },
-                actions = {
-                    IconButton(
-                        onClick = { /*TODO: Implement Profile Screen*/ }
-                    ) {
-                        AsyncImage(
-                            model = profilePicture,
-                            contentDescription = "Profile picture",
-                            modifier = Modifier
-                                .size(40.dp)
-                                .clip(CircleShape)
-                                .border(1.dp, Color.White, CircleShape),
-                            contentScale = ContentScale.Crop
-                        )
-                    }
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
+  Scaffold(
+      topBar = {
+        TopAppBar(
+            title = {
+              Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                Text(
+                    text = "Wildex",
+                    style =
+                        MaterialTheme.typography.titleLarge.copy(
+                            fontWeight = FontWeight.Bold, letterSpacing = 1.sp))
+              }
+            },
+            navigationIcon = {
+              IconButton(onClick = { /*TODO: Implement Notifications Screen*/}) {
+                Icon(
+                    painter = painterResource(R.drawable.notification_bell),
+                    contentDescription = "Notifications",
+                    modifier = Modifier.size(30.dp),
+                )
+              }
+            },
+            actions = {
+              IconButton(onClick = { /*TODO: Implement Profile Screen*/}) {
+                AsyncImage(
+                    model = profilePicture,
+                    contentDescription = "Profile picture",
+                    modifier =
+                        Modifier.size(40.dp)
+                            .clip(CircleShape)
+                            .border(1.dp, Color.White, CircleShape),
+                    contentScale = ContentScale.Crop)
+              }
+            },
+            colors =
+                TopAppBarDefaults.topAppBarColors(
                     containerColor = Color(0xFF082C0B),
                     titleContentColor = Color.White,
-                    navigationIconContentColor = Color.White
-                )
-            )
-        },
-        bottomBar = {
-            //TODO: BottomAppBar() when implementing navigation
-        },
-        content = { pd ->
-            if(posts.isEmpty()){
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(pd)
-                ) {
-                    Icon(
-                        painter = painterResource(R.drawable.nothing_found),
-                        contentDescription = "My custom icon",
-                        tint = Crimson,
-                        modifier = Modifier.size(100.dp)
-                    )
-                    Text(text = "No nearby posts.", style = messageTextStyle)
-                    Text(text = "Start posting...", style = messageTextStyle)
-                }
-            }else {
-                //TODO: Posts
-                LazyColumn(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Top,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(pd)
-                ){
-                    items(posts.size){ index -> PostItem(posts[index]) }
-                }
-            }
+                    navigationIconContentColor = Color.White))
+      },
+      bottomBar = {
+        // TODO: BottomAppBar() when implementing navigation
+      },
+      content = { pd ->
+        if (posts.isEmpty()) {
+          Column(
+              horizontalAlignment = Alignment.CenterHorizontally,
+              verticalArrangement = Arrangement.Center,
+              modifier = Modifier.fillMaxSize().padding(pd)) {
+                Icon(
+                    painter = painterResource(R.drawable.nothing_found),
+                    contentDescription = "My custom icon",
+                    tint = Crimson,
+                    modifier = Modifier.size(100.dp))
+                Text(text = "No nearby posts.", style = messageTextStyle)
+                Text(text = "Start posting...", style = messageTextStyle)
+              }
+        } else {
+          // TODO: Posts
+          LazyColumn(
+              horizontalAlignment = Alignment.CenterHorizontally,
+              verticalArrangement = Arrangement.Top,
+              modifier = Modifier.fillMaxSize().padding(pd)) {
+                items(posts.size) { index -> PostItem(posts[index]) }
+              }
         }
-    )
+      })
 }
 
 @Composable
 fun PostItem(post: Post) {
-    Card(
-        modifier = Modifier.padding(15.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
-        shape = RoundedCornerShape(15.dp)
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(15.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                IconButton(
-                    onClick = { /*TODO: Implement Profile Screen*/ }
-                ) {
-                    AsyncImage(
-                        model = authorPicture,
-                        contentDescription = "Author profile picture",
-                        modifier = Modifier
-                            .size(50.dp)
-                            .clip(CircleShape)
-                    )
+  Card(
+      modifier = Modifier.padding(15.dp),
+      elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
+      shape = RoundedCornerShape(15.dp)) {
+        Column(modifier = Modifier.fillMaxSize()) {
+          Row(
+              modifier = Modifier.fillMaxWidth().padding(15.dp),
+              verticalAlignment = Alignment.CenterVertically) {
+                IconButton(onClick = { /*TODO: Implement Profile Screen*/}) {
+                  AsyncImage(
+                      model = authorPicture,
+                      contentDescription = "Author profile picture",
+                      modifier = Modifier.size(50.dp).clip(CircleShape))
                 }
 
                 Spacer(modifier = Modifier.width(15.dp))
 
                 Column {
-                    Text(
-                        text = "${post.authorId} saw ${post.animalId}",
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 20.sp
-                    )
-                    Text(
-                        text = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault())
-                            .format(post.date.toDate()),
-                        fontSize = 15.sp,
-                        color = Crimson
-                    )
+                  Text(
+                      text = "${post.authorId} saw ${post.animalId}",
+                      fontWeight = FontWeight.Bold,
+                      fontSize = 20.sp)
+                  Text(
+                      text =
+                          SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault())
+                              .format(post.date.toDate()),
+                      fontSize = 15.sp,
+                      color = Crimson)
                 }
 
                 Spacer(modifier = Modifier.width(15.dp))
 
-                IconButton(
-                    onClick = { /*TODO: Implement Post Screen*/ }
-                ) {
+                IconButton(onClick = { /*TODO: Implement Post Screen*/}) {
+                  Icon(
+                      imageVector = Icons.Default.ArrowForward,
+                      contentDescription = "More Info",
+                      modifier = Modifier.fillMaxSize().size(40.dp),
+                      tint = Crimson)
+                }
+              }
+
+          AsyncImage(
+              model = picture,
+              contentDescription = "Post picture",
+              modifier =
+                  Modifier.fillMaxWidth()
+                      .height(200.dp)
+                      .clip(RoundedCornerShape(15.dp))
+                      .padding(horizontal = 15.dp))
+
+          Row(
+              verticalAlignment = Alignment.CenterVertically,
+              horizontalArrangement = Arrangement.SpaceBetween,
+              modifier = Modifier.fillMaxWidth().padding(15.dp)) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                  IconButton(onClick = { /*TODO: Implement Like feature*/}) {
                     Icon(
-                        imageVector = Icons.Default.ArrowForward,
-                        contentDescription = "More Info",
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .size(40.dp),
-                        tint = Crimson
-                    )
-                }
-            }
+                        imageVector = Icons.Default.Favorite,
+                        contentDescription = "Likes",
+                        tint = Crimson)
+                  }
 
-            AsyncImage(
-                model = picture,
-                contentDescription = "Post picture",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
-                    .clip(RoundedCornerShape(15.dp))
-                    .padding(horizontal = 15.dp)
-            )
-
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(15.dp)
-            ) {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    IconButton(
-                        onClick = { /*TODO: Implement Like feature*/ }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Favorite,
-                            contentDescription = "Likes",
-                            tint = Crimson
-                        )
-                    }
-
-                    Text(
-                        text = "${post.likesCount} likes",
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 15.sp,
-                        color = Crimson
-                    )
+                  Text(
+                      text = "${post.likesCount} likes",
+                      fontWeight = FontWeight.Bold,
+                      fontSize = 15.sp,
+                      color = Crimson)
                 }
 
                 Row(verticalAlignment = Alignment.CenterVertically) {
-                    IconButton(
-                        onClick = { /*TODO: Implement Comment Screen*/ }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Menu,
-                            contentDescription = "Comments",
-                            tint = Crimson
-                        )
-                    }
+                  IconButton(onClick = { /*TODO: Implement Comment Screen*/}) {
+                    Icon(
+                        imageVector = Icons.Default.Menu,
+                        contentDescription = "Comments",
+                        tint = Crimson)
+                  }
 
-                    Text(
-                        text = "${post.commentsCount} comments",
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 15.sp,
-                        color = Crimson
-                    )
+                  Text(
+                      text = "${post.commentsCount} comments",
+                      fontWeight = FontWeight.Bold,
+                      fontSize = 15.sp,
+                      color = Crimson)
                 }
-            }
+              }
         }
-    }
+      }
 }
 
 @Preview
 @Composable
 fun HomeScreenPreview() {
-    WildexTheme { Surface( modifier = Modifier.fillMaxSize()) {HomeScreen()} }
+  WildexTheme { Surface(modifier = Modifier.fillMaxSize()) { HomeScreen() } }
 }

66fa4f0531a6b3b20efb501ec7d313551667a3a1
Kozi <steph4140@gmail.com>
2025-10-09 20:07:20 +0200

    feat: Add temporary first draft of Profile Screen

---
+++ app/src/main/java/com/android/wildex/ui/profile/ProfileScreen.kt
@@ -0,0 +1,98 @@
+package com.android.wildex.ui.profile
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.android.wildex.ui.theme.WildexTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.android.wildex.R
+import coil.compose.AsyncImage
+
+@Composable
+fun ProfileScreen(
+    profileScreenViewModel: ProfileScreenViewModel = viewModel()
+) {
+    val uiState by overviewViewModel.uiState.collectAsState()
+    val user = uiState.user
+
+    // Fetch todos when the screen is recomposed
+    LaunchedEffect(Unit) { profileScreenViewModel.refreshUIState() }
+
+    Scaffold(
+        topBar = { /* TODO: Implement TopAppBar */ },
+        content = { pd ->
+            Column(
+                modifier = Modifier.fillMaxSize().padding(pd)
+            ) {
+                Text(text = "Profile Screen Content")
+            }
+        }
+    )
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WildexTopAppBar() {
+    TopAppBar(
+        title = {
+            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                Text(
+                    text = "Wildex",
+                    style =
+                        MaterialTheme.typography.titleLarge.copy(
+                            fontWeight = FontWeight.Bold, letterSpacing = 1.sp, fontSize = 30.sp))
+            }
+        },
+        modifier = Modifier.border(1.dp, WildexGreen).shadow(5.dp),
+        navigationIcon = {
+            IconButton(onClick = { /* TODO: Navigate to Notifications */}) {
+                Icon(
+                    painter = painterResource(R.drawable.notification_bell),
+                    contentDescription = "Notifications",
+                    modifier = Modifier.size(30.dp))
+            }
+        },
+        actions = {
+            IconButton(onClick = { /* TODO: Navigate to Profile */}) {
+                AsyncImage(
+                    model = user.profilePictureURL,
+                    contentDescription = "Profile picture",
+                    modifier =
+                        Modifier.size(40.dp).clip(CircleShape).border(1.dp, WildexGreen, CircleShape),
+                    contentScale = ContentScale.Crop)
+            }
+        },
+        colors =
+            TopAppBarDefaults.topAppBarColors(
+                titleContentColor = WildexGreen, navigationIconContentColor = WildexGreen))
+}
+@Preview
+@Composable
+fun ProfileScreenPreview() {
+    WildexTheme { Surface(modifier = Modifier.fillMaxSize()) { ProfileScreen() } }
+}
---
+++ app/src/main/java/com/android/wildex/ui/profile/ProfileScreenViewModel.kt
@@ -0,0 +1,56 @@
+package com.android.wildex.ui.profile
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.android.wildex.model.user.User
+import com.android.wildex.model.user.UserType
+import com.google.firebase.Timestamp
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+data class ProfileUIState(
+    val user: User? = null,
+    val errorMsg: String? = null,
+    val signedOut: Boolean = false
+)
+class ProfileScreenViewModel(): ViewModel() {
+    private val _uiState = MutableStateFlow(ProfileUIState())
+    val uiState: StateFlow<ProfileUIState> = _uiState.asStateFlow()
+    private val defaultUser: User = User(
+        userId = "defaultUserId",
+        username = "defaultUsername",
+        name = "Default",
+        surname = "User",
+        bio = "This is...",
+        profilePictureURL = "https://example.com/default-profile-pic.png",
+        userType = UserType.REGULAR,
+        creationDate = Timestamp.now(),
+        country = "Nowhere",
+        friendsCount = 0,
+        animalsId = emptyList(),
+        animalsCount = 0,
+        achievementsId = emptyList(),
+        achievementsCount = 0,
+    )
+    fun refreshUIState() {
+        //TODO: fetch user from repository
+        _uiState.value = ProfileUIState(
+            user = fetchUser()
+        )
+    }
+    private fun fetchUser(): User? {
+        var user: User? = null
+        //viewModelScope.launch {
+        try {
+            //TODO: implement fetching user
+            /** _uiState.user = "fetchUserFromUserId(...)" */
+            //user = "to User."Firebase.auth.currentUser
+        } catch (e: Exception) {
+            Log.e("ProfileScreenViewModel", "Error fetching user", e)
+            //setErrorMsg("Failed to load user: ${e.message}")
+        }
+        //}
+        return defaultUser
+    }
+}

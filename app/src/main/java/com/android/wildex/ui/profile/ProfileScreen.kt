0890cf159187bc5852dff7ca8ec639ae6aacdb0f
Kozi <steph4140@gmail.com>
2025-10-10 16:20:51 +0200

    fix: Fix typos and syntax errors

--- app/src/main/java/com/android/wildex/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/android/wildex/ui/profile/ProfileScreen.kt
@@ -22,9 +22,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -32,13 +35,13 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.android.wildex.R
-import coil.compose.AsyncImage
+//import coil.compose.AsyncImage
 
 @Composable
 fun ProfileScreen(
     profileScreenViewModel: ProfileScreenViewModel = viewModel()
 ) {
-    val uiState by overviewViewModel.uiState.collectAsState()
+    val uiState by profileScreenViewModel.uiState.collectAsState()
     val user = uiState.user
 
     // Fetch todos when the screen is recomposed
@@ -68,28 +71,28 @@ fun WildexTopAppBar() {
                             fontWeight = FontWeight.Bold, letterSpacing = 1.sp, fontSize = 30.sp))
             }
         },
-        modifier = Modifier.border(1.dp, WildexGreen).shadow(5.dp),
+        modifier = Modifier.border(1.dp, Color(0xFFD0BCFF) /*WildexGreen*/).shadow(5.dp),
         navigationIcon = {
             IconButton(onClick = { /* TODO: Navigate to Notifications */}) {
-                Icon(
+                /*Icon(
                     painter = painterResource(R.drawable.notification_bell),
                     contentDescription = "Notifications",
-                    modifier = Modifier.size(30.dp))
+                    modifier = Modifier.size(30.dp))*/
             }
         },
         actions = {
             IconButton(onClick = { /* TODO: Navigate to Profile */}) {
-                AsyncImage(
+                /*AsyncImage(
                     model = user.profilePictureURL,
                     contentDescription = "Profile picture",
                     modifier =
                         Modifier.size(40.dp).clip(CircleShape).border(1.dp, WildexGreen, CircleShape),
-                    contentScale = ContentScale.Crop)
+                    contentScale = ContentScale.Crop)*/
             }
         },
-        colors =
+        /*colors =
             TopAppBarDefaults.topAppBarColors(
-                titleContentColor = WildexGreen, navigationIconContentColor = WildexGreen))
+                titleContentColor = WildexGreen, navigationIconContentColor = WildexGreen)*/)
 }
 @Preview
 @Composable

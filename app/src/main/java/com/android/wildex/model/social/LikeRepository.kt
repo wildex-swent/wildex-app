842fc34413c74bf64dc4bf0fbc7395fc13418767
Léonard Lemaire <leonard.lemaire@epfl.ch>
2025-10-10 19:29:07 +0200

    feat: define Like repository interface
    I added the definition of the Like repository interface. It includes
    operations like fetching all likes for a post, adding and deleting likes
    from the repository, or fetching all likes of the current user.

---
+++ app/src/main/java/com/android/wildex/model/social/LikeRepository.kt
@@ -0,0 +1,26 @@
+package com.android.wildex.model.social
+
+/** Represents a repository that manages likes on posts */
+interface LikeRepository {
+
+    /** Generates and returns a new unique identifier for a Like item. */
+    fun getNewLikeId(): String
+
+    /** Retrieves all Like items from the repository. */
+    suspend fun getAllLikes(): List<Like>
+
+    /** Retrieves all Like items made by the current user. */
+    suspend fun getAllLikesByCurrentUser(): List<Like>
+
+    /** Retrieves all Like items for a given Post */
+    suspend fun getLikesForPost(postId: String): List<Like>
+
+    /** Retrieves a specific Like item by its unique identifier. */
+    suspend fun getLike(likeId: String): Like
+
+    /** Adds a new Like item to the repository. */
+    suspend fun addLike(like: Like)
+
+    /** Deletes a Like item from the repository. */
+    suspend fun deleteLike(likeId: String)
+}

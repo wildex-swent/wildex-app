2b6f70d57c29132d08434a79d1599170db67d4b2
zlissane <zeyneb.lissaneeddine@epfl.ch>
2025-10-13 16:20:00 +0200

    feat: temporary activity for testing

--- app/src/main/java/com/android/wildex/MainActivity.kt
+++ app/src/main/java/com/android/wildex/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.android.wildex
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.credentials.CredentialManager
+import com.android.wildex.ui.authentication.SignInScreen
 import com.android.wildex.ui.theme.WildexTheme
 import okhttp3.OkHttpClient
 
@@ -25,8 +29,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun WildexApp() {
-  Text(text = "Welcome to Wildex!")
+fun WildexApp(
+    context: Context = LocalContext.current,
+    credentialManager: CredentialManager = CredentialManager.create(context),
+) {
+  SignInScreen(credentialManager = credentialManager, onSignedIn = { Log.d("Success", "Success") })
 }
 
 @Preview(showBackground = true)

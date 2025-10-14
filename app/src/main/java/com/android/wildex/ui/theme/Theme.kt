89feb55392f9b8e138f8b5b313bd63ec659d2c6d
Rania5724 <rania.hida01@gmail.com>
2025-10-14 14:26:11 +0200

    feat: add color and theme support

--- app/src/main/java/com/android/wildex/ui/theme/Theme.kt
+++ app/src/main/java/com/android/wildex/ui/theme/Theme.kt
@@ -16,29 +16,39 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme =
-    darkColorScheme(primary = Purple80, secondary = PurpleGrey80, tertiary = Pink80)
+    darkColorScheme(
+        primary = LightGreen,
+        secondary = LightBrown,
+        tertiary = LightDarkRed,
+        background = DarkGray,
+        onPrimary = DarkGray,
+        onSecondary = DarkGray,
+        onTertiary = DarkGray,
+        onBackground = White,
+        error = DarkErrorRed,
+        onError = Black,
+    )
 
 private val LightColorScheme =
     lightColorScheme(
-        primary = Purple40, secondary = PurpleGrey40, tertiary = Pink40
-
-        /* Other default colors to override
-        background = Color(0xFFFFFBFE),
-        surface = Color(0xFFFFFBFE),
-        onPrimary = Color.White,
-        onSecondary = Color.White,
-        onTertiary = Color.White,
-        onBackground = Color(0xFF1C1B1F),
-        onSurface = Color(0xFF1C1B1F),
-        */
-        )
+        primary = Green,
+        secondary = Brown,
+        tertiary = DarkRed,
+        background = White,
+        onPrimary = White,
+        onSecondary = White,
+        onTertiary = White,
+        onBackground = Black,
+        error = ErrorRed,
+        onError = White,
+    )
 
 @Composable
 fun WildexTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
   val colorScheme =
       when {

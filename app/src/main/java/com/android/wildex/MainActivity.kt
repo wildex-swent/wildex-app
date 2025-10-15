2fe5cd051b7c4973b8e7c288f8d90b1c2d0405e5
youssef-9511 <youssef.benhayoun05@gmail.com>
2025-10-15 23:40:37 +0200

    chore: remove test screen for merge

--- app/src/main/java/com/android/wildex/MainActivity.kt
+++ app/src/main/java/com/android/wildex/MainActivity.kt
@@ -5,11 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.android.wildex.model.animaldetector.AnimalDetectRepository
-import com.android.wildex.ui.example.AnimalDetectionTestScreen
 import com.android.wildex.ui.theme.WildexTheme
 import okhttp3.OkHttpClient
 
@@ -27,8 +26,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WildexApp() {
-  // Text(text = "Welcome to Wildex!")
-  AnimalDetectionTestScreen(repository = AnimalDetectRepository(HttpClientProvider.client))
+  Text(text = "Welcome to Wildex!")
 }
 
 @Preview(showBackground = true)

1987d175fbbc51ba7c9183c1c2c5c78050a05cc0
zlissane <zeyneb.lissaneeddine@epfl.ch>
2025-10-15 11:53:38 +0200

    feat: add tests for SignInScreen

--- app/src/androidTest/java/com/android/wildex/ui/authentication/SignInScreenTest.kt
+++ app/src/androidTest/java/com/android/wildex/ui/authentication/SignInScreenTest.kt
@@ -1,12 +1,16 @@
 package com.android.wildex.ui.authentication
 
+import android.content.Context
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import androidx.credentials.CredentialManager
 import com.android.wildex.utils.FakeAuthRepository
 import com.android.wildex.utils.FakeCredentialManager
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -15,6 +19,7 @@ class SignInScreenTest {
   private lateinit var fakeCredentialManager: CredentialManager
   private lateinit var fakeRepository: FakeAuthRepository
   private lateinit var viewModel: SignInViewModel
+  private lateinit var context: Context
 
   @get:Rule val composeTestRule = createComposeRule()
 
@@ -55,5 +60,27 @@ class SignInScreenTest {
     composeTestRule.onNodeWithTag(SignInScreenTestTags.LOGIN_BUTTON).assertIsDisplayed()
     composeTestRule.onNodeWithTag(SignInScreenTestTags.WELCOME).assertIsNotDisplayed()
     composeTestRule.onNodeWithTag(SignInScreenTestTags.LOADING_INDICATOR).assertIsNotDisplayed()
+  }
+
+  @Test
+  fun googleSignInButtonClickTriggersOnClick() {
+    var clicked = false
+    composeTestRule.setContent {
+      context = LocalContext.current
+      GoogleSignInButton(context = context, onSignInClick = { clicked = true })
+    }
+
+    composeTestRule.onNodeWithTag(SignInScreenTestTags.LOGIN_BUTTON).performClick()
+    assertTrue(clicked)
+  }
+
+  @Test
+  fun initialTagsAreDisplayed() {
+    composeTestRule.setContent {
+      SignInScreen(authViewModel = viewModel)
+    }
+
+    composeTestRule.onNodeWithTag(SignInScreenTestTags.APP_LOGO).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SignInScreenTestTags.LOGIN_BUTTON).assertIsDisplayed()
   }
 }

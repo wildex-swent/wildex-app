d29ab8a50514fbb602770b3ac68197857226e18b
zlissane <zeyneb.lissaneeddine@epfl.ch>
2025-10-15 23:15:35 +0200

    refactor: move tests from AuthRepositoryFirebaseTest to SignInScreenTest

--- app/src/androidTest/java/com/android/wildex/ui/authentication/SignInScreenTest.kt
+++ app/src/androidTest/java/com/android/wildex/ui/authentication/SignInScreenTest.kt
@@ -4,18 +4,34 @@ import android.content.Context
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.credentials.CredentialManager
 import com.android.wildex.utils.FakeAuthRepository
 import com.android.wildex.utils.FakeCredentialManager
+import com.android.wildex.utils.FakeJwtGenerator
+import com.android.wildex.utils.FirebaseEmulator
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+
+const val UI_WAIT_TIMEOUT = 5_000L
 
 class SignInScreenTest {
+  private lateinit var auth: FirebaseAuth
   private lateinit var fakeCredentialManager: CredentialManager
   private lateinit var fakeRepository: FakeAuthRepository
   private lateinit var viewModel: SignInViewModel
@@ -28,6 +44,16 @@ class SignInScreenTest {
     fakeCredentialManager = FakeCredentialManager.create("fakeToken")
     fakeRepository = FakeAuthRepository()
     viewModel = SignInViewModel(fakeRepository)
+    auth = FirebaseEmulator.auth
+    auth.signOut()
+  }
+
+  @After
+  fun tearDown() {
+    if (FirebaseEmulator.isRunning) {
+      FirebaseEmulator.auth.signOut()
+      FirebaseEmulator.clearAuthEmulator()
+    }
   }
 
   @Test
@@ -80,5 +106,73 @@ class SignInScreenTest {
 
     composeTestRule.onNodeWithTag(SignInScreenTestTags.APP_LOGO).assertIsDisplayed()
     composeTestRule.onNodeWithTag(SignInScreenTestTags.LOGIN_BUTTON).assertIsDisplayed()
+  }
+
+  @Test
+  fun canSignInWithGoogle() {
+    val fakeGoogleIdToken =
+        FakeJwtGenerator.createFakeGoogleIdToken("12345", email = "test@example.com")
+    val fakeCredentialManager = FakeCredentialManager.create(fakeGoogleIdToken)
+
+    val fakeUser = mock(FirebaseUser::class.java)
+    Mockito.`when`(fakeUser.uid).thenReturn("fake-uid")
+
+    fakeRepository.signInResult = Result.success(fakeUser)
+
+    composeTestRule.setContent {
+      SignInScreen(authViewModel = viewModel, credentialManager = fakeCredentialManager)
+    }
+
+    composeTestRule
+        .onNodeWithTag(SignInScreenTestTags.LOGIN_BUTTON)
+        .assertIsDisplayed()
+        .performClick()
+
+    composeTestRule.waitUntil(UI_WAIT_TIMEOUT) {
+      composeTestRule.onNodeWithTag(SignInScreenTestTags.WELCOME).isDisplayed()
+    }
+  }
+
+  @Test
+  fun signInScreenIsCorrectlyDisplayed() {
+    composeTestRule.setContent { SignInScreen() }
+
+    composeTestRule.onNodeWithTag(SignInScreenTestTags.APP_LOGO).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SignInScreenTestTags.LOGIN_BUTTON).assertIsDisplayed()
+  }
+
+  @Test
+  fun canSignInWithExistingAccount() {
+    val email = "existing@test.com"
+    val fakeIdToken =
+        FakeJwtGenerator.createFakeGoogleIdToken(name = "Existing user", email = email)
+    val firebaseCred = GoogleAuthProvider.getCredential(fakeIdToken, null)
+
+    runTest {
+      val user = FirebaseEmulator.auth.signInWithCredential(firebaseCred).await()
+      assertNotNull(user)
+    }
+
+    FirebaseEmulator.auth.signOut()
+
+    val fakeUser = mock(FirebaseUser::class.java)
+    Mockito.`when`(fakeUser.uid).thenReturn("fake-uid")
+    Mockito.`when`(fakeUser.email).thenReturn(email)
+    fakeRepository.signInResult = Result.success(fakeUser)
+
+    composeTestRule.setContent {
+      SignInScreen(authViewModel = viewModel, credentialManager = fakeCredentialManager)
+    }
+
+    composeTestRule
+        .onNodeWithTag(SignInScreenTestTags.LOGIN_BUTTON)
+        .assertIsDisplayed()
+        .performClick()
+
+    composeTestRule.waitUntil(UI_WAIT_TIMEOUT) {
+      composeTestRule.onNodeWithTag(SignInScreenTestTags.WELCOME).isDisplayed()
+    }
+
+    assertEquals(email, fakeUser.email)
   }
 }

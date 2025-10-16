e9ad16f2a49d06eda97bdd4354ed43735fc8c948
Rania5724 <rania.hida01@gmail.com>
2025-10-16 22:01:14 +0200

    feat: set theme dynamic color to false

--- app/src/main/java/com/android/wildex/ui/theme/Theme.kt
+++ app/src/main/java/com/android/wildex/ui/theme/Theme.kt
@@ -47,7 +47,7 @@ private val LightColorScheme =
 fun WildexTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit,
 ) {
   val colorScheme =

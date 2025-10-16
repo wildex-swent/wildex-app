923d6bb6e766d37bf31adf9337f64c2e51837ba3
Kozi <steph4140@gmail.com>
2025-10-16 13:34:34 +0200

    fix: Rewrite UI tests

--- app/src/test/java/com/android/wildex/ui/profile/ProfileScreenTest.kt
+++ app/src/test/java/com/android/wildex/ui/profile/ProfileScreenTest.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.unit.dp
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import coil.ImageLoader
 import coil.compose.LocalImageLoader
 import coil.decode.DataSource
@@ -27,11 +28,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
 
-@RunWith(RobolectricTestRunner::class)
-@Config(sdk = [34])
+@RunWith(AndroidJUnit4::class)
 class ProfileScreenTest {
 
   @get:Rule val composeRule = createComposeRule()

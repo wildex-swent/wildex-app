a475725bde301d3475519468a240e76cbfac9d45
Martin Zimmer <mzimmer@ic2-adrstd-5012.intranet.epfl.ch>
2025-10-17 04:58:10 +0200

    fix: Fromat code

--- app/src/androidTest/java/com/android/wildex/MainActivityTest.kt
+++ app/src/androidTest/java/com/android/wildex/MainActivityTest.kt
@@ -1,17 +1,14 @@
 package com.android.wildex
 
-import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import org.junit.Rule
 import org.junit.Test
 
 class MainActivityTest {
 
-  @get:Rule
-  val composeRule = createAndroidComposeRule<MainActivity>()
+  @get:Rule val composeRule = createAndroidComposeRule<MainActivity>()
 
   @Test
   fun OnCreate() {

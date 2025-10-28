df7e128f9368390d4bb2cb9558def364992b6bd9
Jonathan Pilemand <jonathanpilemand@gmail.com>
2025-10-28 20:09:10 +0100

    feat: add animal information UI and VM skeleton

---
+++ app/src/main/java/com/android/wildex/ui/animal/AnimalInformationScreenViewModel.kt
@@ -0,0 +1,53 @@
+package com.android.wildex.ui.animal
+
+import androidx.lifecycle.ViewModel
+import com.android.wildex.model.utils.Id
+import com.android.wildex.model.utils.URL
+import com.android.wildex.ui.home.defaultUser
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+
+data class AnimalInformationUIState(
+    val animalId: Id = "",
+    val pictureURL: URL = "",
+    val name: String = "",
+    val species: String = "",
+    val description: String = "",
+    val errorMsg: String? = null,
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val isError: Boolean = false,
+    )
+
+
+class AnimalInformationScreenViewModel(
+    // TODO: Add animal repository once implemented
+    private val currentUserId: Id =
+        try {
+            Firebase.auth.uid
+        } catch (_: Exception) {
+            defaultUser.userId
+        } ?: defaultUser.userId,
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(AnimalInformationUIState())
+    val uiState: StateFlow<AnimalInformationUIState> = _uiState.asStateFlow()
+
+    fun clearErrorMsg() {
+        _uiState.value = _uiState.value.copy(errorMsg = null)
+    }
+
+    private fun setErrorMsg(errorMsg: String) {
+        _uiState.value = _uiState.value.copy(errorMsg = errorMsg)
+    }
+
+    fun loadAnimalInformation(animalId: Id){
+        // TODO: set up the UI state
+    }
+
+
+}

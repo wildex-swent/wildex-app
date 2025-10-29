0b8e954998348383ec6ed890d1dce7e9f52c26bc
Kozi <steph4140@gmail.com>
2025-10-29 09:40:04 +0100

    feat: Add basic UIState

--- app/src/main/java/com/android/wildex/ui/profile/EditProfileScreen.kt
+++ app/src/main/java/com/android/wildex/ui/profile/EditProfileScreen.kt
@@ -19,70 +19,66 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.android.wildex.R
 
 object EditProfileScreenTestTags {
-    const val GO_BACK = "edit_profile_screen_go_back_button"
+  const val GO_BACK = "edit_profile_screen_go_back_button"
 }
+
 @Composable
 fun EditProfileScreen(
     editScreenViewModel: EditProfileViewModel = viewModel(),
     onGoBack: () -> Unit = {},
 ) {
-    val cs = colorScheme
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        topBar = {
-            EditProfileTopBar(onGoBack)
-        },
-    ) {
-        pd ->
-        Box(modifier = Modifier.fillMaxSize().padding(pd)) {
-            Button(
-                onClick = {  },
-            ) {
-                Row {
-                    Icon(
-                        imageVector = Icons.Filled.SaveAlt,
-                        contentDescription = "Professional badge",
-                        tint = cs.tertiary,
-                        modifier = Modifier.fillMaxSize(),
-                    )
-                    Text(text = "Save")
-                }
-            }
+  val cs = colorScheme
+  Scaffold(
+      modifier = Modifier.fillMaxSize(),
+      topBar = { EditProfileTopBar(onGoBack) },
+  ) { pd ->
+    Box(modifier = Modifier.fillMaxSize().padding(pd)) {
+      Button(
+          onClick = {},
+      ) {
+        Row {
+          Icon(
+              imageVector = Icons.Filled.SaveAlt,
+              contentDescription = "Professional badge",
+              tint = cs.tertiary,
+              modifier = Modifier.fillMaxSize(),
+          )
+          Text(text = "Save")
         }
+      }
     }
+  }
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditProfileTopBar(onGoBack: () -> Unit) {
-    val cs = colorScheme
-    TopAppBar(
-        title = {
-            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-                Text(
-                    text = "Edit Profile",
-                    fontWeight = FontWeight.SemiBold,
-                    color = cs.onBackground,
-                )
-            }
-        },
-        navigationIcon = {
-            IconButton(
-                modifier = Modifier.testTag(EditProfileScreenTestTags.GO_BACK),
-                onClick = { onGoBack() },
-            ) {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                    contentDescription = "Back",
-                    tint = cs.onBackground,
-                )
-            }
+  val cs = colorScheme
+  TopAppBar(
+      title = {
+        Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+          Text(
+              text = "Edit Profile",
+              fontWeight = FontWeight.SemiBold,
+              color = cs.onBackground,
+          )
         }
-    )
+      },
+      navigationIcon = {
+        IconButton(
+            modifier = Modifier.testTag(EditProfileScreenTestTags.GO_BACK),
+            onClick = { onGoBack() },
+        ) {
+          Icon(
+              imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+              contentDescription = "Back",
+              tint = cs.onBackground,
+          )
+        }
+      })
 }
--- app/src/main/java/com/android/wildex/ui/profile/EditProfileViewModel.kt
+++ app/src/main/java/com/android/wildex/ui/profile/EditProfileViewModel.kt
@@ -1,9 +1,18 @@
 package com.android.wildex.ui.profile
 
 import androidx.lifecycle.ViewModel
+import com.android.wildex.model.user.User
+import com.android.wildex.model.user.UserType
+import com.google.firebase.Timestamp
 
-class EditProfileViewModel(
+private val emptyUser = User("", "", "", "", "", "", UserType.REGULAR, Timestamp(0, 0), "", 0)
 
-): ViewModel() {
+data class EditProfileUIState(
+    val user: User = emptyUser,
+    val isNewUser: Boolean = false,
+    val isLoading: Boolean = false,
+    val errorMsg: String? = null,
+    val isError: Boolean = false,
+)
 
-}
+class EditProfileViewModel() : ViewModel() {}

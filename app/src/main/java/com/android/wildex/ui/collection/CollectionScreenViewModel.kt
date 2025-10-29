70be06e620908d3e7fea042a124ea6978e736f46
Léonard Lemaire <leonard.lemaire@epfl.ch>
2025-10-29 15:35:30 +0100

    fix: remove unused object definitions

--- app/src/main/java/com/android/wildex/ui/collection/CollectionScreenViewModel.kt
+++ app/src/main/java/com/android/wildex/ui/collection/CollectionScreenViewModel.kt
@@ -1,8 +1,6 @@
 package com.android.wildex.ui.collection
 
 import androidx.lifecycle.ViewModel
-import com.android.wildex.model.animal.Animal
-import com.android.wildex.model.user.SimpleUser
 import com.android.wildex.model.utils.Id
 import com.android.wildex.model.utils.URL
 import com.google.firebase.auth.ktx.auth
@@ -10,25 +8,6 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-
-/** Default placeholder user used when no valid user is loaded. */
-val defaultUser: SimpleUser =
-    SimpleUser(
-        userId = "defaultUserId",
-        username = "defaultUsername",
-        profilePictureURL = "",
-    )
-
-/** Default placeholder animal used when no valid animal is associated with a post. */
-val defaultAnimal: Animal =
-    Animal(
-        animalId = "defaultAnimalId",
-        name = "Default Animal",
-        species = "Unknown",
-        description = "This is a default animal.",
-        pictureURL =
-            "https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png",
-    )
 
 data class CollectionUIState(
     val userUid: String = "",

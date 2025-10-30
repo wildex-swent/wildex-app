923659e73ce0a3f48b9f5cd39c657efdaf9ba39a
youssef-9511 <youssef.benhayoun05@gmail.com>
2025-10-31 00:00:34 +0100

    chore: rebase onto origin/main

--- app/src/main/java/com/android/wildex/ui/camera/CameraScreenViewModel.kt
+++ app/src/main/java/com/android/wildex/ui/camera/CameraScreenViewModel.kt
@@ -1,23 +1,34 @@
 package com.android.wildex.ui.camera
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.net.Uri
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.android.wildex.model.RepositoryProvider
 import com.android.wildex.model.animaldetector.AnimalDetectResponse
 import com.android.wildex.model.animaldetector.AnimalInfoRepository
 import com.android.wildex.model.social.Post
 import com.android.wildex.model.social.PostsRepository
+import com.android.wildex.model.storage.StorageRepository
 import com.android.wildex.model.utils.Id
 import com.android.wildex.model.utils.Location
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.Firebase
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.auth
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 data class CameraUiState(
     val animalDetectResponse: AnimalDetectResponse? = null,
-    val postInConstruction: Post? = null,
+    val currentImageUri: Uri? = null,
+    val description: String = "",
+    val addLocation: Boolean = false,
     val errorMsg: String? = null,
     val isLoading: Boolean = false,
     val isDetecting: Boolean = false,
@@ -25,6 +36,7 @@ data class CameraUiState(
 
 class CameraScreenViewModel(
     private val postsRepository: PostsRepository = RepositoryProvider.postRepository,
+    private val storageRepository: StorageRepository = RepositoryProvider.storageRepository,
     private val animalRepository: AnimalInfoRepository = RepositoryProvider.animalInfoRepository,
     // private val collectionRepository: CollectionRepository =
     // RepositoryProvider.collectionRepository,
@@ -33,21 +45,83 @@ class CameraScreenViewModel(
   private val _uiState = MutableStateFlow(CameraUiState())
   val uiState: StateFlow<CameraUiState> = _uiState.asStateFlow()
 
+  /* resets to default state where camera preview is shown */
   fun resetState() {
-    /* resets to default state where camera preview is shown */
+    _uiState.value = CameraUiState()
   }
 
-  fun detectAnimalImage(imageUri: Uri) {
-    /* detects animal in the image, and creates postInConstruction */
+  /* detects animal in the image */
+  fun detectAnimalImage(imageUri: Uri, context: Context) {
+    _uiState.value = _uiState.value.copy(isDetecting = true)
+    viewModelScope.launch {
+      try {
+        val animalDetectResponse = animalRepository.detectAnimal(context, imageUri).first()
+        _uiState.value =
+            _uiState.value.copy(
+                animalDetectResponse = animalDetectResponse,
+                currentImageUri = imageUri,
+                isDetecting = false,
+            )
+        Log.d("CameraScreenViewModel", "Animal detected: ${animalDetectResponse.animalType}")
+      } catch (e: Exception) {
+        setErrorMsg("Failed to detect animal and start post creation : ${e.message}")
+        resetState()
+      }
+    }
   }
 
+  /* updates the image uri of the post in construction, keeps it across config changes */
+  fun updateImageUri(imageUri: Uri) {
+    _uiState.value = _uiState.value.copy(currentImageUri = imageUri)
+  }
+
+  /* updates the description of the post in construction, keeps it across config changes */
   fun updateDescription(description: String) {
-    /* updates the description of the post in construction, keeps it across config changes */
+    _uiState.value = _uiState.value.copy(description = description)
   }
 
-  fun createPost(location: Location, onPost: () -> Unit) {
-    /* calls `registerAnimal` and creates a post with the detected animal and current user,
-    with the location taken from the composable, and triggers onPost when it is posted */
+  /* toggles whether to add location to the post in construction */
+  fun toggleAddLocation() {
+    _uiState.value = _uiState.value.copy(addLocation = !_uiState.value.addLocation)
+  }
+
+  /* calls `registerAnimal` and creates a post with the detected animal and current user,
+  with the location taken from the composable, and triggers onPost when it is posted */
+  @SuppressLint("MissingPermission")
+  fun createPost(context: Context, onPost: () -> Unit) {
+    val uri = uiState.value.currentImageUri ?: return
+    val response = uiState.value.animalDetectResponse ?: return
+    _uiState.value = _uiState.value.copy(isLoading = true)
+    viewModelScope.launch {
+      try {
+        val postId = postsRepository.getNewPostId()
+        val imageUrl = storageRepository.uploadPostImage(postId, uri)!!
+        val animalId = response.taxonomy.id
+        val location =
+            LocationServices.getFusedLocationProviderClient(context).lastLocation.await().let {
+              Location(it.latitude, it.longitude)
+            }
+        val finalPost =
+            Post(
+                postId = postId,
+                authorId = currentUserId!!,
+                description = uiState.value.description,
+                location = location,
+                date = Timestamp.now(),
+                pictureURL = imageUrl,
+                animalId = animalId,
+                likesCount = 0,
+                commentsCount = 0,
+            )
+        postsRepository.addPost(finalPost)
+        registerAnimal(animalId)
+        resetState()
+        onPost()
+      } catch (e: Exception) {
+        setErrorMsg("Failed to create post : ${e.message}")
+        _uiState.value = _uiState.value.copy(isLoading = false)
+      }
+    }
   }
 
   /** Clears any existing error message from the UI state. */
@@ -60,7 +134,22 @@ class CameraScreenViewModel(
     _uiState.value = _uiState.value.copy(errorMsg = msg)
   }
 
-  private fun registerAnimal() {
-    /* registers the animal from the animal response in the repository if not already there */
+  /* registers the animal from the animal response in the repository if not already there */
+  private suspend fun registerAnimal(animalId: Id) {
+    val detection = uiState.value.animalDetectResponse ?: return
+    val animalDescription = animalRepository.getAnimalDescription(detection.animalType)
+    /*
+    val animalPicture = animalRepository.getAnimalPicture(detection.animalType)
+    val animalPictureURL = storageRepository.uploadAnimalPicture(animalId,
+    animalPic)
+    val animal = Animal(
+        animalId,
+        animalPictureId,
+        detection.animalType,
+        detection.taxonomy.species,
+        animalDescription
+    )
+    collectionRepository.addAnimalIfNotExists(animal)
+    */
   }
 }

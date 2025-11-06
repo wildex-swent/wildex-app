586c31f98f38a70f946458bf8c7604af710da1c3
zlissaneeddine <zeyneb.lissaneeddine@epfl.ch>
2025-11-06 19:45:37 +0100

    fix: format using ktfmt

--- app/src/main/java/com/android/wildex/ui/achievement/AchievementsScreen.kt
+++ app/src/main/java/com/android/wildex/ui/achievement/AchievementsScreen.kt
@@ -1,6 +1,5 @@
 package com.android.wildex.ui.achievement
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column

c061c5579d7a2688a10dc540400eebd2501a61cb
Rania5724 <rania.hida01@gmail.com>
2025-11-07 23:36:06 +0100

    feat: map screen implementation
    # Conflicts:
    #	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/com/android/wildex/ui/map/MapCanvas.kt
@@ -0,0 +1,65 @@
+package com.android.wildex.ui.map
+
+import androidx.compose.material3.ColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import com.mapbox.common.toValue
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.MapView
+import com.mapbox.maps.extension.compose.MapEffect
+import com.mapbox.maps.extension.compose.MapboxMap
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
+import com.mapbox.maps.plugin.locationcomponent.location
+
+data class MapUiColors(
+    val bg: Color, // container color
+    val fg: Color // content color
+)
+
+@Composable
+fun MapCanvas(
+    modifier: Modifier,
+    mapViewRef: (MapView) -> Unit,
+    styleUri: String,
+    styleImportId: String,
+    isDark: Boolean,
+    showUserLocation: Boolean,
+    indicatorListener: OnIndicatorPositionChangedListener,
+) {
+  MapboxMap(modifier = modifier.testTag(MapContentTestTags.MAP_CANVAS)) {
+    MapEffect(isDark, showUserLocation, styleUri, styleImportId) { mv ->
+      mapViewRef(mv)
+      val mapboxMap = mv.mapboxMap
+      mapboxMap.loadStyle(styleUri) { style ->
+        mapboxMap.setCamera(
+            CameraOptions.Builder().center(Point.fromLngLat(6.6323, 46.5197)).zoom(12.0).build())
+        runCatching {
+          style.setStyleImportConfigProperty(
+              styleImportId,
+              "lightPreset",
+              (if (isDark) "night" else "day").toValue(),
+          )
+        }
+      }
+      mv.location.updateSettings {
+        enabled = showUserLocation
+        pulsingEnabled = showUserLocation
+      }
+      mv.location.removeOnIndicatorPositionChangedListener(indicatorListener)
+      if (showUserLocation) {
+        mv.location.addOnIndicatorPositionChangedListener(indicatorListener)
+      }
+    }
+  }
+}
+
+/** One source of truth for Map tab/card colors. */
+fun colorsForMapTab(tab: MapTab, cs: ColorScheme): MapUiColors =
+    when (tab) {
+      MapTab.Posts -> MapUiColors(cs.primary, cs.onPrimary)
+      MapTab.MyPosts -> MapUiColors(cs.secondary, cs.onSecondary)
+      MapTab.Reports -> MapUiColors(cs.tertiary, cs.onTertiary)
+    }

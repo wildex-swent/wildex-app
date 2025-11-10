3c550532d24e84157b83dc6a04dbb1c72c7d7145
leolmr1110 <leonardlemaire2006@gmail.com>
2025-11-10 12:37:01 +0100

    fix: fix Collection screen
    I fixed the bugs and implemented some new suggestions in the
    Collection screen. These new changes include:
    - Removing the top bar when the screen is loading
    - Removing the locked animals from the collection when viewing another
    user's collection
    - Switching the name of locked animals to "???" to keep the mystery when
    viewing your own collection
    - Fixing images display mode so that they always adapt to the card's
    dimensions
    - Setting a dynamic height to the animals' cards so that each row of
    animals takes up 25% of the screen's height for comfort of use and
    visibility. I used `LocalConfiguration.current.screenHeightDp.dp` to get
    the current device's screen height.

--- app/src/main/java/com/android/wildex/ui/collection/CollectionScreen.kt
+++ app/src/main/java/com/android/wildex/ui/collection/CollectionScreen.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -34,6 +36,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
@@ -106,12 +109,14 @@ fun CollectionScreen(
       modifier = Modifier.fillMaxSize(),
       bottomBar = { bottomBar() },
       topBar = {
-        CollectionTopBar(
-            uiState.isUserOwner,
-            uiState.user.profilePictureURL,
-            onGoBack,
-            onProfileClick,
-            onNotificationClick)
+        if (!uiState.isLoading) {
+          CollectionTopBar(
+              uiState.isUserOwner,
+              uiState.user.profilePictureURL,
+              onGoBack,
+              onProfileClick,
+              onNotificationClick)
+        }
       }) { innerPadding ->
         Box(modifier = Modifier.padding(innerPadding)) {
           when {
@@ -198,13 +203,18 @@ fun CollectionTopBar(
  */
 @Composable
 fun AnimalsView(animalsStates: List<AnimalState>, onAnimalClick: (Id) -> Unit) {
+  val screenHeight = LocalConfiguration.current.screenHeightDp.dp
   LazyColumn(modifier = Modifier.fillMaxSize().testTag(CollectionScreenTestTags.ANIMAL_LIST)) {
     val nbRows = ceil(animalsStates.size / 2.0).toInt()
+    val rowHeight = (screenHeight) / 4
     items(nbRows) { index ->
       val rowStartIndex = index * 2
       Row(
           horizontalArrangement = Arrangement.spacedBy(20.dp),
-          modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp, vertical = 10.dp),
+          modifier =
+              Modifier.height(rowHeight)
+                  .fillMaxWidth()
+                  .padding(horizontal = 20.dp, vertical = 10.dp),
       ) {
         AnimalView(animalsStates[rowStartIndex], onAnimalClick, modifier = Modifier.weight(1f))
         // Check if there is another animal to display when we are at the last row
@@ -237,26 +247,30 @@ fun AnimalView(animalState: AnimalState, onAnimalClick: (Id) -> Unit, modifier: 
             modifier.testTag(
                 CollectionScreenTestTags.testTagForAnimal(
                     animalState.animalId, animalState.isUnlocked))) {
-          AsyncImage(
-              model = animalPictureURL,
-              contentDescription = animalName,
-              modifier = Modifier.size(180.dp).clip(RoundedCornerShape(8.dp)),
-              contentScale = ContentScale.Crop,
-          )
-          Text(
-              text = animalName,
-              fontWeight = FontWeight.Medium,
-              color = colorScheme.background,
-              textAlign = TextAlign.Center,
-              fontSize = 17.sp,
-              style =
-                  TextStyle(
-                      lineHeight = 2.2.em,
-                      lineHeightStyle =
-                          LineHeightStyle(
-                              alignment = LineHeightStyle.Alignment.Center,
-                              trim = LineHeightStyle.Trim.None)),
-              modifier = Modifier.fillMaxSize().background(color = colorScheme.primary))
+          Column(modifier = Modifier.fillMaxSize()) {
+            AsyncImage(
+                model = animalPictureURL,
+                contentDescription = animalName,
+                modifier = Modifier.fillMaxSize().weight(0.8f),
+                contentScale = ContentScale.Crop)
+
+            Text(
+                text = if (animalState.isUnlocked) animalName else "???",
+                fontWeight = FontWeight.Medium,
+                color = colorScheme.background,
+                textAlign = TextAlign.Center,
+                fontSize = 17.sp,
+                style =
+                    TextStyle(
+                        lineHeight = 2.2.em,
+                        lineHeightStyle =
+                            LineHeightStyle(
+                                alignment = LineHeightStyle.Alignment.Center,
+                                trim = LineHeightStyle.Trim.None)),
+                modifier =
+                    Modifier.fillMaxWidth().weight(0.2f).background(color = colorScheme.primary),
+            )
+          }
         }
     if (!animalState.isUnlocked) {
       Box(

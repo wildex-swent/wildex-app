25640418be4f64613c322d05d2072c213393457b
Martin Zimmer <mzimmer@ic2-adrstd-5006.intranet.epfl.ch>
2025-11-12 15:34:31 +0100

    fix: Rename field in the Comment data class

--- app/src/main/java/com/android/wildex/model/social/CommentRepository.kt
+++ app/src/main/java/com/android/wildex/model/social/CommentRepository.kt
@@ -11,6 +11,9 @@ interface CommentRepository {
   /** Retrieves all Comment items associated with a specific post. */
   suspend fun getAllCommentsByPost(postId: Id): List<Comment>
 
+  /** Retrieves all Comment items associated with a specific report. */
+  suspend fun getAllCommentsByReport(reportId: Id): List<Comment>
+
   /** Adds a new Comment item to the repository. */
   suspend fun addComment(comment: Comment)
 

7207c105ec8b80154327e36bc618f6822c5bcbca
Stéphane Buloncelli <164889704+MyKozi@users.noreply.github.com>
2025-11-18 18:03:26 +0100

    feat: add notification screen and viewmodel skeleton (#246)
    ## Description
    This PR implements the skeleton for the notification screen and its
    viewModel. It will be accessed through the homepage's topbar when the
    following PR implements its full behavior.
    
    ## Related issues
    Closes #246 
    
    ## Notes
    As this is still a skeleton PR, no tests have been implemented. They
    will be added in the next PR.

---
+++ app/src/main/java/com/android/wildex/ui/notification/NotificationScreen.kt
@@ -0,0 +1,23 @@
+package com.android.wildex.ui.notification
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun NotificationScreen(
+    onGoBack: () -> Unit = {},
+    notificationScreenViewModel: NotificationScreenViewModel = viewModel(),
+) {
+  Scaffold(
+      modifier = Modifier.fillMaxSize(),
+      topBar = { NotificationTopBar(onGoBack = onGoBack) },
+  ) { pd ->
+    NotificationView(pd)
+  }
+}
+
+@Composable fun NotificationView(pd: PaddingValues) {}
---
+++ app/src/main/java/com/android/wildex/ui/notification/NotificationTopBar.kt
@@ -0,0 +1,54 @@
+package com.android.wildex.ui.notification
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.android.wildex.R
+import com.android.wildex.ui.profile.EditProfileScreenTestTags
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NotificationTopBar(onGoBack: () -> Unit) {
+  val cs = colorScheme
+  CenterAlignedTopAppBar(
+      title = {
+        Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+          Text(
+              text = LocalContext.current.getString(R.string.notification_title),
+              fontWeight = FontWeight.SemiBold,
+              color = cs.onBackground,
+          )
+        }
+      },
+      navigationIcon = {
+        IconButton(
+            modifier = Modifier.testTag(EditProfileScreenTestTags.GO_BACK),
+            onClick = { onGoBack() },
+        ) {
+          Icon(
+              imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+              contentDescription = "Back",
+              tint = cs.onBackground,
+          )
+        }
+      },
+      actions = {
+        // Empty box to center the title
+        Box(modifier = Modifier.size(48.dp))
+      })
+}

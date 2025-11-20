88ed82f8f2f089d3805c6bcb7e4bcd79d2adb1d0
Léonard Lemaire <leonard.lemaire@epfl.ch>
2025-11-20 14:39:01 +0100

    feat: add sendRequestToUser function

--- app/src/main/java/com/android/wildex/ui/social/FriendScreenViewModel.kt
+++ app/src/main/java/com/android/wildex/ui/social/FriendScreenViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.android.wildex.model.RepositoryProvider
 import com.android.wildex.model.relationship.Relationship
 import com.android.wildex.model.relationship.RelationshipRepository
+import com.android.wildex.model.relationship.StatusEnum
 import com.android.wildex.model.user.SimpleUser
 import com.android.wildex.model.user.UserFriendsRepository
 import com.android.wildex.model.user.UserRepository
@@ -19,9 +20,10 @@ import kotlinx.coroutines.launch
 data class FriendsScreenUIState(
   val friends: List<FriendState> = emptyList(),
   val suggestions: List<SuggestionState> = emptyList(),
-  val receivedRequests: List<RequestState> = emptyList(),
-  val sentRequests: List<RequestState> = emptyList(),
+  val receivedRequests: List<Relationship> = emptyList(),
+  val sentRequests: List<Relationship> = emptyList(),
   val isCurrentUser: Boolean = false,
+  val isRefreshing: Boolean = false,
   val isLoading: Boolean = false,
   val isError: Boolean = false,
   val errorMsg: String? = null,
@@ -39,10 +41,6 @@ data class SuggestionState(
   val isPending: Boolean
 )
 
-data class RequestState(
-  val request: Relationship,
-)
-
 class FriendScreenViewModel(
   private val currentUserId: Id = Firebase.auth.uid ?: "",
   private val userRepository: UserRepository = RepositoryProvider.userRepository,
@@ -56,24 +54,105 @@ class FriendScreenViewModel(
   /** Public immutable state exposed to the UI layer. */
   val uiState: StateFlow<FriendsScreenUIState> = _uiState.asStateFlow()
 
-  private suspend fun updateUIState() {
+  private suspend fun updateUIState(userId: Id) {
     try {
+      val isCurrentUser = userId == currentUserId
+      val userFriends = userFriendsRepository.getAllFriendsOfUser(userId)
+      val currentUserFriends = userFriendsRepository.getAllFriendsOfUser(currentUserId)
+      val currentUserSentRequests = friendRequestRepository.getAllPendingRelationshipsBySender(currentUserId)
+      val friendsStates = userFriends.map {
+        FriendState(
+          friend = userRepository.getSimpleUser(it),
+          isFriend = currentUserFriends.contains(it),
+          isPending = currentUserSentRequests.any { request -> request.receiverId == it }
+        )
+      }
+      //val suggestions = if (isCurrentUser) userRecommender.getRecommendedUsers() else emptyList()
+      val receivedRequests = if (isCurrentUser){
+        friendRequestRepository.getAllPendingRelationshipsByReceiver(currentUserId)
+      } else emptyList()
+      val sentRequests = if (isCurrentUser){
+        friendRequestRepository.getAllPendingRelationshipsBySender(currentUserId)
+      } else emptyList()
       _uiState.value =
         _uiState.value.copy(
+          friends = friendsStates,
+          //suggestions = suggestions,
+          receivedRequests = receivedRequests,
+          sentRequests = sentRequests,
+          isCurrentUser = isCurrentUser,
+          isRefreshing = false,
           isLoading = false,
           errorMsg = null,
           isError = false,
         )
     } catch (e: Exception) {
-      setErrorMsg(e.localizedMessage ?: "Failed to load posts.")
-      _uiState.value = _uiState.value.copy(isLoading = false, isError = true)
+      setErrorMsg(e.localizedMessage ?: "Failed to load friends and requests.")
+      _uiState.value = _uiState.value.copy(isRefreshing = false, isLoading = false, isError = true)
     }
   }
 
-  fun loadUIState() {
+  fun loadUIState(userId: Id) {
     _uiState.value = _uiState.value.copy(isLoading = true, errorMsg = null, isError = false)
-    viewModelScope.launch { updateUIState() }
+    viewModelScope.launch { updateUIState(userId) }
   }
+
+  fun refreshUIState(userId: Id) {
+    _uiState.value = _uiState.value.copy(isRefreshing = true, errorMsg = null, isError = false)
+    viewModelScope.launch { updateUIState(userId) }
+  }
+
+  fun sendRequestToUser(userId: Id){
+    viewModelScope.launch {
+      val state = _uiState.value
+      val request = Relationship(
+        senderId = currentUserId,
+        receiverId = userId,
+        status = StatusEnum.PENDING
+      )
+      val sentRequests = state.sentRequests
+      val newSentRequests = if (state.isCurrentUser) sentRequests + listOf(request) else sentRequests
+      val friends = state.friends
+      val newFriends = if (state.isCurrentUser){
+        friends.filter {
+          it.friend.userId != userId
+        }
+      } else {
+        friends.map {
+          if (it.friend.userId == userId) {
+            it.copy(isPending = true)
+          } else it
+        }
+      }
+      val suggestions = state.suggestions
+      val newSuggestions = if (state.isCurrentUser){
+        suggestions.filter {
+          it.suggestedUser.userId != userId
+        }
+      } else suggestions
+
+      _uiState.value = state.copy(
+        friends = newFriends,
+        sentRequests = newSentRequests,
+        suggestions = newSuggestions
+      )
+
+      try{
+        friendRequestRepository.initializeRelationship(currentUserId, userId)
+      } catch (e: Exception){
+        setErrorMsg("Failed to send request to user $userId : ${e.message}")
+        _uiState.value = state
+      }
+    }
+  }
+
+  fun unfollowUser(userId: Id){}
+
+  fun acceptReceivedRequest(userId: Id){}
+
+  fun declineReceivedRequest(userId: Id){}
+
+  fun cancelSentRequest(userId: Id){}
 
 
   /** Clears any existing error message from the UI state. */

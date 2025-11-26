dc002d80036db25ad59e4f347b233fdaf485cc6b
Jonathan Pilemand <jonathanpilemand@gmail.com>
2025-11-26 15:24:11 +0100

    fix: remove unused import

--- app/src/main/java/com/android/wildex/ui/collection/CollectionScreen.kt
+++ app/src/main/java/com/android/wildex/ui/collection/CollectionScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api

23e5fecef92a2199e0b98a1d5cd438022106a8e5
zlissaneeddine <zeyneb.lissaneeddine@epfl.ch>
2025-11-27 18:16:36 +0100

    fix: format using ktmft

--- app/src/main/java/com/android/wildex/ui/report/ReportScreen.kt
+++ app/src/main/java/com/android/wildex/ui/report/ReportScreen.kt
@@ -43,7 +43,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel

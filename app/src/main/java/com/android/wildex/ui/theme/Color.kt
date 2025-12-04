e7b90c2e51f50cd4573e5283b0997adc127d415f
Jonathan Pilemand <jonathanpilemand@gmail.com>
2025-12-04 23:26:29 +0100

    feat: add surface variant color

--- app/src/main/java/com/android/wildex/ui/theme/Color.kt
+++ app/src/main/java/com/android/wildex/ui/theme/Color.kt
@@ -4,10 +4,12 @@ import androidx.compose.ui.graphics.Color
 
 // Light Theme Colors
 val WildexDarkGreen = Color(0xFF2e6f40)
+val WildexLightSurface = Color(0xFFf5f5f5)
 val ErrorRed = Color(0xFFFF404C)
 
 // Dark Theme Colors
 val WildexLightGreen = Color(0xFF2fab68)
+val WildexDarkSurface = Color(0xFF1d3449)
 val DarkErrorRed = Color(0xFFbd000d)
 
 // Common Colors

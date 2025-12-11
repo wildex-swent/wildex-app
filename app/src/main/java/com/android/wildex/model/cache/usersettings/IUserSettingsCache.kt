41837af48f7fb701c21450b2e261261ccfaee0d9
zlissaneeddine <zeyneb.lissaneeddine@epfl.ch>
2025-12-12 00:23:19 +0100

    fix: add userId parameter

--- app/src/main/java/com/android/wildex/model/cache/usersettings/IUserSettingsCache.kt
+++ app/src/main/java/com/android/wildex/model/cache/usersettings/IUserSettingsCache.kt
@@ -1,40 +1,41 @@
 package com.android.wildex.model.cache.usersettings
 
 import com.android.wildex.model.user.AppearanceMode
+import com.android.wildex.model.utils.Id
 
 /** Cache interface for user settings. */
 interface IUserSettingsCache {
   /** Initializes the user settings in the cache with default values. */
-  suspend fun initializeUserSettings()
+  suspend fun initializeUserSettings(userId: Id)
 
   /**
    * Retrieves the notification enablement setting.
    *
    * @return Boolean indicating if notifications are enabled, or null if not set.
    */
-  suspend fun getEnableNotification(): Boolean?
+  suspend fun getEnableNotification(userId: Id): Boolean?
 
   /**
    * Sets the notification enablement setting.
    *
    * @param enable Boolean indicating if notifications should be enabled.
    */
-  suspend fun setEnableNotification(enable: Boolean)
+  suspend fun setEnableNotification(userId: Id, enable: Boolean)
 
   /**
    * Retrieves the appearance mode setting.
    *
    * @return AppearanceMode indicating the current appearance mode, or null if not set.
    */
-  suspend fun getAppearanceMode(): AppearanceMode?
+  suspend fun getAppearanceMode(userId: Id): AppearanceMode?
 
   /**
    * Sets the appearance mode setting.
    *
    * @param mode AppearanceMode to be set.
    */
-  suspend fun setAppearanceMode(mode: AppearanceMode)
+  suspend fun setAppearanceMode(userId: Id, mode: AppearanceMode)
 
   /** Clears all user settings from the cache. */
-  suspend fun clear()
+  suspend fun clear(userId: Id)
 }
